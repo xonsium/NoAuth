af69cb73d7c92c6f12c065dbde2af2ce2f9cfe14
Faridin Islam <faridin05saif@gmail.com>
2024-07-24 16:06:09 +0600

    Fixed header in error

--- demo.py
+++ demo.py
@@ -1,6 +1,5 @@
 from noauth import NoAuth
 
 api = NoAuth(log=True)
-api.nauth()
 data = api.query("https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n")
 print(data)
--- noauth.py
+++ noauth.py
@@ -6,10 +6,12 @@ import os
 class NoAuth():
 
     def __init__(self, log) -> None:
+        self.log = log
         if os.path.isfile("session.json"):
             with open("session.json", 'r') as f:
                 file_json = json.load(f)
                 self.access_token = file_json.get('accessToken')
+                print(self.access_token)
                 self.cliend_id = file_json.get('clientId')
         else:
             self.auth()
@@ -27,6 +29,7 @@ class NoAuth():
 
         self.access_token = session_json.get('accessToken')
         self.cliend_id = session_json.get('clientId')
+        if self.log: print(f"Access Token: {self.access_token}\nClient ID: {self.cliend_id}")
 
 
     def query(self, url):
@@ -35,9 +38,13 @@ class NoAuth():
         }
         req = urllib.request.Request(url, None, headers)
         try:
+            print(self.access_token)
             res = urllib.request.urlopen(req).read()
         except urllib.error.HTTPError:
             self.auth()
+            headers = {
+            "Authorization": f"Bearer {self.access_token}"
+            }
             res = urllib.request.urlopen(req).read()
         json_res = json.loads(res)
         return json_res
